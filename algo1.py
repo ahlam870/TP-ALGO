fb28a07173538ab47f545b5bca146c6697c8fb1f
HADJALA AHLAM <hdahlam442@gmail.com>
2024-12-05 20:34:23 +0100

    TP2

--- algo1.py
+++ algo1.py
@@ -33,3 +33,45 @@ print("Avant la construction du tas max :")
 print(A)
 
 tas_max(A)
+
+
+
+# part 2
+
+def tri(T):
+    n = len(T)
+
+    for i in range(n // 2 - 1, -1, -1):
+        entasser_min(T, n, i)
+
+    for i in range(n - 1, 0, -1):
+        T[0], T[i] = T[i], T[0]
+
+        entasser_min(T, i, 0)
+    return T
+
+def entasser_min(T, n, i):
+    min_idx = i  
+    g = 2 * i + 1  # FG
+    d = 2 * i + 2  # FD
+
+    if g < n and T[g] < T[min_idx]:
+        min_idx = g
+
+    if d < n and T[d] < T[min_idx]:
+        min_idx = d
+
+    if min_idx != i:
+        T[i], T[min_idx] = T[min_idx], T[i]
+        entasser_min(T, n, min_idx)
+
+AR = [10, 22, 5, 18, 20, 25, 40, 30, 35, 12]
+
+print("Avant le tri par tas :")
+print(AR)
+
+AR_trie = tri(AR)
+
+print("Après le tri par tas (décroissant) :")
+print(AR_trie)
+
